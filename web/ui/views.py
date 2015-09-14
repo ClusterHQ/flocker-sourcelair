7cc8a193d3c8104eefc3d490cad1236c0dd770af
Antonis Kalipetis <akalipetis@gmail.com>
2015-09-15 00:12:55 +0300

    Added login templates

--- web/ui/views.py
+++ web/ui/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def register_view(request):
     return render(request, 'ui/register.html')
+
+def login_view(request):
+    return render(request, 'ui/login.html')
