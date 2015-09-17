48cc38c8768b8ca4f0157e8f78642646e0db305e
aabeer <j0nky.puff@gmail.com>
2015-09-17 17:25:38 +0300

    fixing login and login.js

--- web/ui/static/ui/js/login.js
+++ web/ui/static/ui/js/login.js
@@ -1,36 +1,42 @@
 $('form.login-form').on('submit', function() {
-  var form = $(this);
-  var url = '/api/auth/login/';
-  var data = {};
-  var errCondition = false;
+	var form = $(this);
+	var url = '/api/auth/login/';
+	var data = {};
+	var errCondition = false;
+	console.log("HEY");
 
-  //Loop through and grab only the value of the attribute id.
-  form.find('[id]').each(function() {
-    var element = $(this);
-    var idValue = element.attr('id');
-    var value = element.val();
+	//Loop through and grab only the value of the attribute id.
+	form.find('[id]').each(function() {
+		var element = $(this);
+		var idValue = element.attr('id');
+		var value = element.val();
 
-    data[idValue] = value;
-  });
+		data[idValue] = value;
+	});
 
-  $.ajax({
-    url   : url,
-    type  : 'POST',
-    data  : data,
-  }).done(function(data, textStatus, req) {
-      // Place code here to redirect to /terminal
-      window.location.href = '/terminal';
-  }).fail(function(req, textStatus, errorThrown) {
-      // Place code here to show error from req.responseJSON object
-      // Form key: error
-	  if(errCondition == true)
-		$('div.alert').removbe();
-      var JSONresponse = req.responseJSON;
-      for( var key in JSONresponse){
-        var warning = '<div class="alert alert-warning alert-block space fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Form ' + key + ' error :</strong> ' + JSONresponse[key] + '</div>';
-        $('.error-warnings').append(warning);
-      }
-  });
-  errCondition = true;
-  return false; //Prevent default.
+	$.ajax({
+		url   : url,
+		type  : 'POST',
+		data  : data,
+	}).done(function(data, textStatus, req) {
+		// Place code here to redirect to /terminal
+		window.location.href = '/terminal';
+	}).fail(function(req, textStatus, errorThrown) {
+		// Place code here to show error from req.responseJSON object
+		// Form key: error
+		if(errCondition == true)
+			$('div.alert').remove();
+		var JSONresponse = req.responseJSON;
+		console.log(textStatus);
+		console.log(errorThrown);
+		var errorThrownMsg = '<div class="alert alert-warning alert-block space fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>' + errorThrown + ' : ' + ' </strong> Enter username or password right. </div>';
+
+		for( var key in JSONresponse){
+			var warning = '<div class="alert alert-warning alert-block space fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Form ' + key + ' error :</strong> ' + JSONresponse[key] + '</div>';
+			$('.error-warnings').append(warning);
+		}
+		$('.error-warnings').append(errorThrownMsg);
+	});
+	errCondition = true;
+	return false; //Prevent default.
 });
--- web/ui/urls.py
+++ web/ui/urls.py
@@ -4,4 +4,5 @@ from ui import views
 
 urlpatterns = [
     url(r'^$', views.register_view),
+    url(r'^login/$', views.login_view),
 ]
