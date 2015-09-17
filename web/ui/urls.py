d7e8a9835f10b60b086fe0e945646e2b37ccca4a
Antonis Kalipetis <akalipetis@gmail.com>
2015-09-17 17:38:23 +0300

    Reformatted JS/HTML code

--- web/ui/urls.py
+++ web/ui/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from ui import views
 
 urlpatterns = [
-    url(r'^$', views.register_view),
-    url(r'^login/$', views.login_view),
+    url(r'^$', views.register_view, name='register'),
+    url(r'^login/$', views.login_view, name='login'),
 ]
