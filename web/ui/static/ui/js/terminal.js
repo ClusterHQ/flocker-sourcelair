5ec9d52a2b636e2ac3c7f8843348f76afbfb49d4
aabeer <j0nky.puff@gmail.com>
2015-09-22 11:52:04 +0300

    Added terminal
    Added fit.js

--- web/ui/static/ui/js/terminal.js
+++ web/ui/static/ui/js/terminal.js
@@ -1,1 +1,33 @@
 console.log('Add terminal related code here!');
+var terminalContainer = document.getElementById('terminal-container'),
+    term = new Terminal(),
+    shellprompt = '> ';
+
+term.open(terminalContainer);
+term.fit();
+
+term.prompt = function () {
+  term.write('\r\n' + shellprompt);
+};
+
+term.writeln('Welcome to xterm.js');
+term.writeln('Just type some keys in the prompt below.');
+term.writeln('');
+term.prompt();
+
+term.on('key', function (key, ev) {
+  var printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
+
+  if (ev.keyCode == 13) {
+    term.prompt();
+  } else if (ev.keyCode == 8) {
+    /*
+     * Do not delete the prompt
+     */
+    if (term.x > 2) {
+      term.write('\b \b');
+    }
+  } else if (printable) {
+    term.write(key);
+  }
+});
