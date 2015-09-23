569bf9434088868b033b60288c8635f66877b1ed
aabeer <j0nky.puff@gmail.com>
2015-09-23 18:01:15 +0300

    added some basic style.

--- web/ui/static/ui/js/terminal.js
+++ web/ui/static/ui/js/terminal.js
@@ -14,6 +14,7 @@ function getContainer() {
         data: data,
     }).done(function(data, textStatus, req) {
         attachToTerminal(data.attach_url);
+        displayContainerInfo(data.container_meta_data);
     }).fail(function(req, textStatus, errorThrown) {
         // Notify user that we have a problem.
     });
@@ -24,6 +25,14 @@ function attachToTerminal(webSocketUrl) {
     var params = '?logs=1&stream=1',
         ws = new WebSocket(webSocketUrl + params);
     term.attach(ws, true, true);
+}
+
+function displayContainerInfo(info) {
+    var name = info.Node.Name,
+        ip = info.Node.IP,
+        alert = $('#container-info');
+
+    alert.html('Container created at node: <strong>' + name + '</strong> with IP: <strong>' + ip + '</strong>.');
 }
 
 (function(){
