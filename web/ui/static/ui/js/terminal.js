958470ad41333b646ca46b372f543ac1b248957e
Antonis Kalipetis <akalipetis@gmail.com>
2015-09-23 16:23:30 +0300

    Added .editorconfig and reformatted terminal.js

--- web/ui/static/ui/js/terminal.js
+++ web/ui/static/ui/js/terminal.js
@@ -5,37 +5,17 @@ var terminalContainer = document.getElementById('terminal-container'),
 term.open(terminalContainer);
 term.fit();
 
-term.prompt = function () {
-  term.write('\r\n' + shellprompt);
-};
-
 term.writeln('Welcome to flocker-sourcelair project!');
 term.writeln('Here is your fully functional terminal, have fun.');
 term.writeln(" _____                          _           _      \r\n"+
-"/  ___|                        | |         (_)     \r\n"+
-"\\ `--.  ___  _   _ _ __ ___ ___| |     __ _ _ _ __ \r\n"+
-" `--. \\/ _ \\| | | | '__/ __/ _ \\ |    / _` | | '__|\r\n"+
-"/\\__/ / (_) | |_| | | | (_|  __/ |___| (_| | | |   \r\n"+
-"\\____/ \\___/ \\__,_|_|  \\___\\___\\_____/\\__,_|_|_|   \r\n"+
-"                                                   \r\n");
-term.prompt();
+             "/  ___|                        | |         (_)     \r\n"+
+             "\\ `--.  ___  _   _ _ __ ___ ___| |     __ _ _ _ __ \r\n"+
+             " `--. \\/ _ \\| | | | '__/ __/ _ \\ |    / _` | | '__|\r\n"+
+             "/\\__/ / (_) | |_| | | | (_|  __/ |___| (_| | | |   \r\n"+
+             "\\____/ \\___/ \\__,_|_|  \\___\\___\\_____/\\__,_|_|_|   \r\n"+
+             "                                                   \r\n");
 
-term.on('key', function (key, ev) {
-  var printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
 
-  if (ev.keyCode == 13) {
-    term.prompt();
-  } else if (ev.keyCode == 8) {
-    /*
-     * Do not delete the prompt
-     */
-    if (term.x > 2) {
-      term.write('\b \b');
-    }
-  } else if (printable) {
-    term.write(key);
-  }
-});
 
 (function(){
     term.toggleFullscreen();
